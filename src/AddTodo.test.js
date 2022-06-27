914ac3ca16cdcac9b9e289daa583d308aa46af8f
zeng28 <zeng28@illinois.edu>
2022-06-26 23:53:38 -0500

    Completed testing, restored About section.

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -19,25 +19,89 @@ afterEach(() => {
 
 
 
- test('test that App component doesn\'t render dupicate Task', () => {
+ test('test that App component doesn\'t render duplicate Task', () => {
   render(<App />);
+
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByRole('textbox', {name: /Due Date/i});
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  fireEvent.change(inputTask, {target: {value: "History Test"}});
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(element);
+
+  fireEvent.change(inputTask, {target: {value: "History Test"}});
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(element);
+
+  const check = screen.getAllByText(/History Test/i);
+  expect(check.length).toBe(1);
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
+
+  const inputDate = screen.getByRole('textbox', {name: /Due Date/i});
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(element);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
 
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
+
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const element = screen.getByRole('button', {name: /Add/i});
+
+  fireEvent.change(inputTask, {target: {value: "History Test"}});
+  fireEvent.click(element);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
-
-
 
  test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
+
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByRole('textbox', {name: /Due Date/i});
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  fireEvent.change(inputTask, {target: {value: "History Test"}});
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(element);
+
+  const deleteByCheck = screen.getByRole('checkbox');
+  fireEvent.click(deleteByCheck);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
 
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByRole('textbox', {name: /Due Date/i});
+  const element = screen.getByRole('button', {name: /Add/i});
+
+  fireEvent.change(inputTask, {target: {value: "History Test"}});
+  fireEvent.change(inputDate, {target: {value: "05/30/2023"}});
+  fireEvent.click(element);
+
+  fireEvent.change(inputTask, {target: {value: "Maths Test"}});
+  fireEvent.change(inputDate, {target: {value: "05/30/2020"}});
+  fireEvent.click(element);
+
+  const check = screen.getByTestId(/History Test/i).style.background;
+  const checkOverdue = screen.getByTestId(/Maths Test/i).style.background;
+  expect (check == checkOverdue).toBe(false);
  });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
-    if (this.state.todos.find(element => element.content === todo.content)){
+    if (this.state.todos.find(element => element.content === todo.content) || "Invalid Date" === todo.due || null === todo.due){
       return
     } else {
       todo.id = Math.random();
